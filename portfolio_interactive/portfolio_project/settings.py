28dfb472ab1161a9dfd115cc23801cc6d6cf88ab
Валентин Юркин <iurkinvalentin92@gmail.com>
2025-01-15 17:28:21 +0300

    static

--- portfolio_interactive/portfolio_project/settings.py
+++ portfolio_interactive/portfolio_project/settings.py
@@ -132,3 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1,
 }
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
