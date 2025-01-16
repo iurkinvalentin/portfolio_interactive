65cb8ab29214c80b06c869e064d5fda9045ad134
Валентин Юркин <iurkinvalentin92@gmail.com>
2025-01-16 20:03:59 +0300

    admin

--- portfolio_interactive/portfolio/admin.py
+++ portfolio_interactive/portfolio/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ContactFormSubmission
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ('title',)
+
+
+@admin.register(ContactFormSubmission)
+class ContactFormSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'message', 'submitted_at')  # Поля, отображаемые в списке
+    list_filter = ('submitted_at',)  # Фильтр по дате
+    search_fields = ('name', 'email', 'message')  # Поля для поиска
+    ordering = ('-submitted_at',)  # Сортировка по убыванию даты
